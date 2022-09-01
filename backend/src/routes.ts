07304e6124e4fee8091ce01ea67cc44fe5048aee
unknown <igormedeiros.f@gmail.com>
2022-08-31 22:01:24 -0300

    middleware de auth + endpoint get user details

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import {Router} from 'express'
 import { createUserController } from './controllers/user/UserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 
 
@@ -8,5 +10,7 @@ const router = Router()
 // USER ROUTES
 router.post('/register', new createUserController().handle)
 router.post('/login', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export {router}
