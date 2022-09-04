a087433c6e60c27310a092442dd41974cb2dc9ed
unknown <igormedeiros.f@gmail.com>
2022-09-04 18:30:19 -0300

    finalizando html e css do frontend

--- frontend/projeto-pizzaria/src/pages/index.tsx
+++ frontend/projeto-pizzaria/src/pages/index.tsx
@@ -1,10 +1,53 @@
+import Head from 'next/head'
 
+// Logo
+import Image from 'next/image'
+// CSS
+import styles from '../../styles/home.module.scss'
+
+import logoImg from '../../public/logo.svg'
+// Components
+import { Input } from '../components/ui/Input/Input'
+import { Button } from '../components/ui/Button/Button'
 
 
 export default function Home() {
   return (
-    <div>
-    <h1>Sujeito Pizza</h1>
-    </div>
+    <>
+      <Head>
+        <title>
+          Sujeito Pizza - Faça seu login
+        </title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='Logo Sujeiro Pizzaria' />
+
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder='Digite seu e-mail'
+              type='email'
+            />
+
+            <Input
+              placeholder='Digite sua senha'
+              type='password'
+            />
+
+
+            <Button 
+            type='submit'
+            loading={false}
+            >
+              Acessar
+            </Button>
+          </form>
+
+          <a className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </a>
+        </div>
+      </div>
+    </>
   )
 }
