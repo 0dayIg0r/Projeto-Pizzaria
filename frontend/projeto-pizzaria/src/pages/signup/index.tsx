bf5e8be3e7b5073127e6561d4565182d1f614150
unknown <igormedeiros.f@gmail.com>
2022-09-04 21:59:56 -0300

    page signup concluida

--- frontend/projeto-pizzaria/src/pages/signup/index.tsx
+++ frontend/projeto-pizzaria/src/pages/signup/index.tsx
@@ -3,29 +3,34 @@ import Head from 'next/head'
 // Logo
 import Image from 'next/image'
 // CSS
-import styles from '../../styles/home.module.scss'
+import styles from '../../../styles/home.module.scss'
 
-import logoImg from '../../public/logo.svg'
+import logoImg from '../../../public/logo.svg'
 // Components
-import { Input } from '../components/ui/Input/Input'
-import { Button } from '../components/ui/Button/Button'
+import { Input } from '../../components/ui/Input/Input'
+import { Button } from '../../components/ui/Button/Button'
 
 // Next Link
 import Link from 'next/link'
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
         <title>
-          Sujeito Pizza - Faça seu login
+          Sujeito Pizza - Faça seu cadastro agora
         </title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='Logo Sujeiro Pizzaria' />
 
         <div className={styles.login}>
+        <h1>Craindo sua conta</h1>
           <form>
+          <Input
+              placeholder='Digite seu nome'
+              type='text'
+            />
             <Input
               placeholder='Digite seu e-mail'
               type='email'
@@ -41,13 +46,13 @@ export default function Home() {
             type='submit'
             loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href='/signup'>
+          <Link href='/'>
           <a className={styles.text}>
-            Não possui uma conta? <b>Cadastre-se</b>
+            Já possui uma conta? <b>Fazer login</b>
           </a>
           </Link>
         </div>
