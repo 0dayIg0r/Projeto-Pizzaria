fb891930a7362d9800165ed7e08b7d33ac04a631
unknown <igormedeiros.f@gmail.com>
2022-09-08 22:08:39 -0300

    privando rota de categoria

--- frontend/projeto-pizzaria/src/pages/category/index.tsx
+++ frontend/projeto-pizzaria/src/pages/category/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '../../components/Header'
 import styles from './styles.module.scss'
 import { setupAPIClient } from '../../services/api'
 import { toast } from 'react-toastify'
+import { canSSRAuth } from '../../utils/canSSR.Auth'
 
 export default function Category() {
     const [name, setName] = useState('')
@@ -58,3 +59,9 @@ export default function Category() {
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async ()=>{
+    return{
+        props:{}
+    }
+})
