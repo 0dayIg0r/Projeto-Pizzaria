65c4bbac46c1f355b0fbc0f5500b2c29bcf0de5d
unknown <igormedeiros.f@gmail.com>
2022-09-09 18:36:07 -0300

    request para ver pedidos

--- frontend/projeto-pizzaria/src/pages/dashboard/index.tsx
+++ frontend/projeto-pizzaria/src/pages/dashboard/index.tsx
@@ -1,23 +1,67 @@
+import { useState } from 'react'
 import { canSSRAuth } from '../../utils/canSSR.Auth'
-
 import Head from 'next/head'
 import { Header } from '../../components/Header'
+import styles from './styles.module.scss'
+import { FiRefreshCcw } from 'react-icons/fi'
+import { setupAPIClient } from '../../services/api'
 
-export default function DashBoard(){
-    return(
+
+type OrderProps = {
+    id: string,
+    table: string,
+    status: boolean,
+    draft: boolean,
+    name: string | null
+}
+interface HomeProps {
+    orders: OrderProps[]
+}
+
+export default function DashBoard({ orders }: HomeProps) {
+    const [orderList, setOrderList] = useState(orders || [])
+
+    const handleModal = (id: string) =>{
+        
+    }
+    return (
         <>
             <Head>
                 <title> Painel - Sujeito Pizzaria</title>
             </Head>
 
-            <Header/>
-            <h1>Painel</h1>
+            <Header />
+
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw size={25} color='#3FFAA3' />
+                    </button>
+                </div>
+
+                <article className={styles.listOrders}>
+                    {orderList.map((item) => (
+                        <section className={styles.orderItem} key={item.id}>
+                            <button onClick={() => handleModal(item.id)}>
+                                <div className={styles.tag}></div>
+                                <span>Mesa {item.table}</span>
+                            </button>
+                        </section>
+                    ))}
+                </article>
+            </main>
         </>
     )
 }
 
-export const getServerSideProps = canSSRAuth(async (ctx) =>{
-    return{
-        props:{}
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx)
+    const res = await apiClient.get('/order')
+
+    return {
+        props: {
+            orders: res.data
+        }
     }
 })
