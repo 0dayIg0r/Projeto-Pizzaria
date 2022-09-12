c3ba44c302294cbacaf94d2c1a6eff894494f006
unknown <igormedeiros.f@gmail.com>
2022-09-11 23:15:09 -0300

    iniciando mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor='#1D1D2E'  translucent={false}  />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
