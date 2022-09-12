20a3b5bd39f30cb5e0eb9d1f769097e821819a14
unknown <igormedeiros.f@gmail.com>
2022-09-11 23:27:20 -0300

    iniciando paginas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SingIn from './src/pages/SignIn';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor='#1D1D2E'  translucent={false}  />
+      <SingIn/>
     </View>
   );
 }
---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function SingIn() {
+  return (
+    <View>
+        <Text>
+            MA OEE
+        </Text>
+    </View>
+  )
+}
+
+export default SingIn
