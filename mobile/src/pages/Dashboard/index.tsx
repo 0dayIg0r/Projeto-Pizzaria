71c419b6da009ed85144d87507e6f603cc70d81c
unknown <igormedeiros.f@gmail.com>
2022-09-14 15:12:52 -0300

    abrindo mesa + iniciando interface da página de pedidos

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,22 +1,42 @@
-import React, { useContext } from 'react'
-import { View, Text, SafeAreaView, TouchableOpacity, TextInput,StyleSheet } from 'react-native'
-import { AuthContext } from '../../contexts/AuthContext'
+import React, { useState } from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StackParamList } from '../../routes/app.routes'
+import { NativeStackNavigationProp} from '@react-navigation/native-stack'
+
+
 
 
 function Dashboard() {
-  const { signOut } = useContext(AuthContext)
+  // Typing navigation
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>()
+
+  const [number, setNumber] = useState('')
+
+  async function openOrder() {
+    if (number === '') {
+      return
+    }
+
+    // Make the request and navigate to next page
+    navigation.navigate('Order', {number: number, order_id: 'a1s1d23550x2x15a5w11'})
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Novo pedido</Text>
 
       <TextInput
-      style={styles.input}
-      placeholder='Número da mesa'
-      placeholderTextColor= '#F0F0F0'
-      keyboardType='numeric'
+        style={styles.input}
+        placeholder='Número da mesa'
+        placeholderTextColor='#F0F0F0'
+        keyboardType='numeric'
+        value={number}
+        onChangeText={setNumber}
       />
 
-      <TouchableOpacity style={styles.button}> 
+      <TouchableOpacity style={styles.button} onPress={openOrder}>
         <Text style={styles.buttonText}>Abrir mesa</Text>
       </TouchableOpacity>
     </SafeAreaView>
@@ -24,20 +44,20 @@ function Dashboard() {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems:'center',
+    alignItems: 'center',
     paddingVertical: 15,
     backgroundColor: '#1D1D2E'
   },
-  title:{
+  title: {
     fontSize: 30,
     fontWeight: 'bold',
     color: '#FFF',
     marginBottom: 24
   },
-  input:{
+  input: {
     width: '90%',
     height: 60,
     backgroundColor: '#101026',
@@ -47,7 +67,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: '#FFF'
   },
-  button:{
+  button: {
     width: '90%',
     height: 40,
     backgroundColor: '#3FFFA3',
@@ -56,7 +76,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonText:{
+  buttonText: {
     fontSize: 18,
     color: '#101026',
     fontWeight: 'bold'
---
+++ mobile/src/pages/Order/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+
+
+
+function Order() {
+  return (
+    <View style={styles.container}>
+        <Text>Tela order</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
+
+export default Order
